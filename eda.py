8edad8a53d9e4a43383271f03a100d55113ba072
EliorM <elior.mihaelashvili@gmail.com>
2025-04-05 14:06:09 +0300

    eda initialization

---
+++ eda.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.decomposition import PCA
+import plotly.express as px
+import plotly.graph_objects as go
+import missingno as msno
+# from pandas_profiling import ProfileReport
+
